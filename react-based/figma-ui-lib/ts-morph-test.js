8100fc03643a38a662ce9d75d51f768879e4fd2f
jonathanshnyder <contact@metaexplorer.io>
2021-08-21 09:38:34 +0200

    found some useful ts API functions

--- react-based/figma-ui-lib/ts-morph-test.js
+++ react-based/figma-ui-lib/ts-morph-test.js
@@ -6,3 +6,8 @@ var sourceFiles = project.addSourceFilesAtPaths("lib/**/*{.d.ts,.ts}");
 //const sourceFile = project.getSourceFileOrThrow("./lib/Button.d.ts");
 var personInterface = sourceFiles[2].getInterfaces();
 console.log("bla " + personInterface[0].getName());
+//prints the key on the interface
+console.log(personInterface[0].getMembers()[1].getSymbol().getName());
+//prints "__type", if that member doesn't have a named type, otherwise the named type
+console.log(personInterface[0].getMembers()[1].getType().getSymbol().getName());
+console.log(personInterface[0].getMembers()[1].getStructure());
--- react-based/figma-ui-lib/ts-morph-test.ts
+++ react-based/figma-ui-lib/ts-morph-test.ts
@@ -6,6 +6,23 @@ const project = new Project();
 const sourceFiles = project.addSourceFilesAtPaths("lib/**/*{.d.ts,.ts}");
 //const sourceFile = project.getSourceFileOrThrow("./lib/Button.d.ts");
 
-const personInterface = sourceFiles[1].getInterfaces();
+const personInterface = sourceFiles[2].getInterfaces();
 
 console.log("bla " + personInterface[0].getName());
+//prints the key on the interface
+console.log(personInterface[0].getMembers()[1].getSymbol().getName());
+//prints "__type", if that member doesn't have a named type, otherwise the named type
+console.log(personInterface[0].getMembers()[1].getType().getSymbol().getName());
+/**
+ * returns this for the `interface IBla {onLogin: () => void}`
+ * {
+  name: 'onLogin',
+  type: '() => void',
+  initializer: undefined,
+  hasQuestionToken: false,
+  isReadonly: false,
+  docs: [],
+  kind: 31
+}
+ */
+console.log(personInterface[0].getMembers()[1].getStructure());
