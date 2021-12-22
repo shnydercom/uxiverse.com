9ab260fb8efc41af68d7cbd42cff76b0f8ecbecb
shnyder <ajs.private@gmail.com>
2021-12-22 15:50:49 +0100

    current starter project w/ react doesn't compile either

---
+++ react-based/figma-plugin-playground/src/app/components/Header-from-UI-lib.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { Header } from "@uxiverse.com/ui-lib";
+
+export const HeaderFromUILib = () => {
+  return (
+    <Header
+      onLogin={() => {}}
+      onCreateAccount={() => {}}
+      onLogout={() => {}}
+    >
+    </Header>
+  );
+};
