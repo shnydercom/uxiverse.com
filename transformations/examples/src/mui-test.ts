34b14e897cefb80816372e47f3a24fc2e470e2a9
shnyder <ajs.private@gmail.com>
2021-12-24 16:22:20 +0100

    more experiments and debugger

--- transformations/examples/src/mui-test.ts
+++ transformations/examples/src/mui-test.ts
@@ -1,4 +1,4 @@
-import { Project } from "ts-morph";
+import { Project, ts, createWrappedNode } from "ts-morph";
 
 //const project = new Project( {skipFileDependencyResolution: false, skipLoadingLibFiles: false});
 
@@ -15,30 +15,72 @@ console.log(defaultImportType.getBaseTypes())
 for (const property of defaultImportType.getProperties()) {
   console.log(property.getName());
 }*/
+const project = new Project({
+  skipFileDependencyResolution: false,
+  skipLoadingLibFiles: false,
+});
 
-const project = new Project( {skipFileDependencyResolution: false, skipLoadingLibFiles: false});
-
-const sourceFile = project.createSourceFile("file.ts", `
+const sourceFile = project.createSourceFile(
+  "file.ts",
+  `
 import muiLib from "@mui/material";
-`);
+`
+);
 
 for (const importDeclaration of sourceFile.getImportDeclarations()) {
-    const moduleSpecifierSymbol = importDeclaration.getModuleSpecifier().getSymbolOrThrow();
-    for (const symbol of moduleSpecifierSymbol.getExports()) {
-      //console.log(symbol.getName());
-      if(/*symbol.getName() === "StyledEngineProvider" ||*/ symbol.getName() === "Button"){
-        //console.log(symbol.getMembers());
-        //console.log(symbol.getExports());
-        console.log(symbol.compilerSymbol.declarations![0].getChildren() ?? "bla" );
-        //console.log(symbol.getAliasedSymbol()?.getDeclarations()[0].getFirstChild()?.getType());
-        //console.log(symbol.getAliasedSymbol()?.getValueDeclaration()?.getParent());
-        //console.log(symbol.getDeclarations()[0].getType());
-        //console.log(symbol.getFlags());
-        //console.log(symbol.getFullyQualifiedName());
-        //gets the @mui/material/index.ts console.log(symbol.getDeclarations()[0].getSourceFile());
-      }
+  //returns "@mui/material"
+  const moduleSpecifierValue = importDeclaration.getModuleSpecifierValue();
+  const moduleSourceFile = importDeclaration
+    .getModuleSpecifierSourceFile()
+    ?.getText();
+  //wheter (the first 4) imports inside @mui/material/index.ts are relative or not
+  const isImpRelativeArray = importDeclaration
+    .getModuleSpecifierSourceFile()
+    ?.getImportDeclarations()
+    .map((val) => val.isModuleSpecifierRelative());
+  //gets (the first 4) imports inside @mui/material/index.ts; ["react","@mui/types","./styles","./colors",]
+  const moduleSpecifierValueArray = importDeclaration
+    .getModuleSpecifierSourceFile()
+    ?.getImportDeclarations()
+    .map((val) => val.getModuleSpecifierValue());
+  const moduleSpecifierSymbol = importDeclaration
+    .getModuleSpecifier()
+    .getSymbolOrThrow();
+  for (const symbol of moduleSpecifierSymbol.getExports()) {
+    //console.log(symbol.getName());
+    if (
+      /*symbol.getName() === "StyledEngineProvider" ||*/ symbol.getName() ===
+      "Button"
+    ) {
+      // These all return false
+      const isFunctionDecl = ts.isFunctionDeclaration(symbol.getDeclarations()[0] as unknown as ts.Node);
+      const isJSXExpression = ts.isJsxExpression(symbol.getDeclarations()[0] as unknown as ts.Node);
+      const isJSXElement = ts.isJsxElement(symbol.getDeclarations()[0] as unknown as ts.Node);
+      
+      const getMembers = symbol.getMembers();
+      const getExports = symbol.getExports();
+      const compilerSymbol =
+        symbol.compilerSymbol.declarations![0].getChildren() ?? "bla";
+      const defNodes = symbol.getAliasedSymbol()?.getDeclarations();
+      const getAliasedSymbolPlusType = symbol
+        .getAliasedSymbol()
+        ?.getDeclarations()[0]
+        .getFirstChild()
+        ?.getType();
+      const getAliasedSymbolParent = symbol
+        .getAliasedSymbol()
+        ?.getValueDeclaration()
+        ?.getParent();
+      const getDeclarations = symbol.getDeclarations()[0].getType();
+
+      const getFlags = symbol.getFlags();
+      //still the same source file as moduleSourceFile:
+      const impDecl = symbol.getDeclarations()[0].getSourceFile();
+      const getFullyQualifiedName = symbol.getFullyQualifiedName();
+      //gets the @mui/material/index.ts console.log(symbol.getDeclarations()[0].getSourceFile());
     }
   }
+}
 
 //console.log(sourceFiles[0].getExportSymbols()[0].compilerSymbol.declarations)
 /*
