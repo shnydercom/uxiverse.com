151acd97dc5b1cbb74234161f72bcfd21c459e19
jonathanshnyder <contact@metaexplorer.io>
2022-08-13 17:34:09 +0200

    first version of recommendations-FSM done

--- react-based/figma-plugin/src/state/mainMachine.ts
+++ react-based/figma-plugin/src/state/mainMachine.ts
@@ -71,7 +71,7 @@ export interface MainMachineState {
 }
 
 export const mainMachine =
-  /** @xstate-layout N4IgpgJg5mDOIC5QFsCGBLAdgOgC4Ht8AbXdABwGVdVcxsIwAzVAVxIBVCTyBiACQDyANQCiAJQD6AVQCSEkQBkRAWXkA5duMSgy+WOlL5M2kAA9EAWgDMABgDs2AJwBGK3YAcdxzavv3AFncAGhAAT0t-R2wANn9nR0dou387G3cAVgAmdIBfHJC0LDwuUkpqWmxYMjAAY3RGdBrOYlL+YXFpOUUVeQANGXYTXX1DYyQzS0yE7F9s+P9Ix3904LDEf2xIwK9MwMznd2jfPIKMHAALPVwKMCJa0aoaOkx8G7ua0baKdgkZDXEAIIAYXYMgEagkFG6IIkyikClBQz0BnQRhM5gQ1iszmwmWiK3cznS6R8nmcIXCCCsmSs2B8WSWB0J-hs-hOIEKFyub3uqMwjwqLx5Hz5Xx+f00YmBoPBkOhPwofwA4kokSM+ejLFYrOknI47K4kpEMnZ0lYKYgrNEcVZAgtPMl8UTouzOdhLrBrrdeUYBXQAE6oADuyjYpGFnyhShhAAUxCIhGqUWjxhjMpkHP5MjZ0+4lo4MzmLQgaTZsM4UvrUv5tc4prl8hyzu7ud6Rb7ygHg6HuBHRVGRDC1CJeoNxsNk2NQGmMzNlniSY4AtEEsWM+5yzYV9rqfE7JlDq7mx6ve8Hp3sIGQ2H0H2jGKJAAREQDkFJ0aahA4o7uW2+PNWIkTrmmsJaRLi+rageARWoER5FCed78heV49uGbafII3y-P8UogmCEKvgqyqquOyIfqmlipNg6TWqkBz0ds5KgYEG7-tiSQ2Ac6ZWPBXKekhfqVFgUB3DItDIDImBobeGGilh4q4dKBFytGPxwgiMjvhqlFUrYc4Zi4+wrAezGUlmOI1tEK7RLsBrpM4dguo2bqIXJHZPMJmCiWA4lgJJ0k3khD4SoC+GykRkIkSI2kptOiBZJZvjEqa6ZpLsxYpDiGQ5qkdh2NSObOHxLYCe5yGefo3liRJUkycFClPi+8qxVOEwIOkSy4g5CQrCyBVFqB8Tfl4Bo2I4nXWSuJVuWefJCVeFAiXcDUCNhz5Ea1n4WI5G7OKyNauKk2RpP4xZHFEE2-iuNLRDYaS8S5x6tnNHkVIty1gKt2GhXhMqEfKsLwoiZHqnF7U7bqrg1lutoOekCzpHYxbEtEmw3Qa0TuPd40lf6tT4MgyBgJgEA0PNF4vGIBNEyTZOfCIj4DBIQICMoMZQgCYhAnwW26TtOrYF4rIJISRz7QsxaElENITQxWapGaeM08TpPk29zz4NTNSE6r9OiozzPxmoALKOIIgxgowIxaDk7bSSG7jd4zj7ayqQ5iBlLOzR0GK-uWOEsrOu02r56eZAKLeUChOUGAqD+jU5x+jwABifwyBQfDyEzPys+znPc7ztsUfFmJRDYtH7UjS45g5nuIH4G4rksWbY4rKRB7rdPqxVFQR6Q3nU5gqDE9TZBEKgNRfRe-lkLgoRDyPYBjxPU88IbCrsFz7AxnwUpQnzpcWH4MxI6afj3W4hKrJSLvZnSd2AQWivxM5pxFPjwd6z3Qn9yJi+jzAOPSe09PKfy7qTESjx-S4BjOcQMsAwDrxzrCGQj5HxKF3vvG2OhyI6VLtkWkNlsbXSyDSakxYXaEmwJ4BYeIuLpgLJ3EO+sNbYD-oPEmS8V4gIWirOmIllDoAgBAO4cCEFIMEKISQAAhMQAgADqUIJDxjzmbNQj4AT-UPu1akdIPCxA8HiCshxsiUOpFEOIFddj7WxE5N+TYP78NDhTcOEBI5QAAcvIBq9QHvWce47yQiRFiPgagRBPAADSIgACacYXwUAkHIxRyjVFs3UZo7Rxd8HtX8EkGhiRDTjU4o4cxUxNhV3sFxFckRMjMO-mHPu7iB6eK4YA4BU8+FfwEUE4RoiwDiPCUg1mag05iFUMoNBGCLZ7wBAfbJ4MMT7iiEjWYLtrIDWiJQ3YG5Fi0UAl4BYrh6nd0aXQDhrTh7tN8UJaoYAADWgzEGPiYFgScPARljImVMzBsz5m4LBm1DEtZywFRdo4WwbgCq-koYEWk+wazYj-BNOpT0nHdJcWwi5XieGdM7KndOmds5GxECbM28ZLbWx0RiawtEYh1jhdmFI2ZWTFiXDaRGELrSsjcGydkLwGDwHGG6AgLRyBCQYMwMMzRuBkGpREPMc4PDLEiNaW0WzQIWGJDEOIS45Z12tIkEqorZVCSqLUeojQZWlHlZiU0QsnIZDyXmJkhJixat1LEeIeZ4aGlRe-HAJrSh+ltVq1wSqAgI0Keq91dYHCxBZfEHi+I-AzRej6XumskKhsCDiNIRwVw5hhnmYs2oHB5XcDSJyRJTpprKq9TNl5uxBXKralIThOqATNNZM0TqzqgTLULewla3DWgdnygNpVTwZrNZ9PyAV6qtoWUChKAQYgFSONqewdYdSlrcEOoxo6a0BDrdO9sjaPrVS+kugFdtdLODRqycalZK1FtsudOImx7rLASEcYk+pT2CU7KGkkup81Wm8LsWwJahrxHXYkJcxIoP7kepO8BLCf6Uy1gEnuob-zlmdssStjkHZSxpELVwKwljZkcjqE5mLG0XOjsgWO8dE6-xY-PZjrGE7nFtfQoWNjEj5XGlkG+iAjI0XsLWaknhiP0dYYx5pIluM3DY0nYDy7Py3UE7qpIqQUXiYQAEMsJIwV5mMlkBsaGcNnPYcpzhVzvEdL8eczjC82nOd8Xh7EThsixC3cuew-bb4uw2FuHwXhsS0QPKhxxOB0MNNcU0jxOKfG8JQgEqB1AYFPLAPxytPtIj3SSN4U0lCwsP1ZBWExWMtwKcw241LnncWucvFl3pISBlhMQQV2kw65PeDzD+5GQ0Tp0hcC4eY6YVzWfi+1jFinf4Ocudw9LeLPJ3MeT1sALyGiYDvbekuuiH00SJHXO+NIswatvoBNGyy0iJBggkf183EunOS+clbaWXNCUYGwIgeXbUeF2axA8+wiSUdhflXEFZbSmlgscNFCXbOffs81pzrWQ1af5jqXUBZaKItOndZIlDWSZCq7MOrtg0gNbOSBvRTsuKuxSJfYsyQHCywcljEk9g7B8RA3SpnLtv3u3rpieIUQE1cRrPWCaSs8g5CAA */
+  /** @xstate-layout N4IgpgJg5mDOIC5QFsCGBLAdgOgC4Ht8AbXdABwGVdVcxsIwAzVAVxIBVCTyBiACQDyANQCiAJQD6AVQCSEkQBkRAWXkA5duMSgy+WOlL5M2kAA9EAWgDMABgDs2AJwBGK3YAcdxzavv3AFncAGhAAT0t-R2wANn9nR0dou387G3cAVgAmdIBfHJC0LDwuUkpqWmxYMjAAY3RGdBrOYlL+YXFpOUUVeQANGXYTXX1DYyQzS0yE7F9s+P9Ix3904LDEf2xIwK9MwMznd2jfPIKMHAALPVwKMCJa0aoaOkx8G7ua0baKdgkZDXEAIIAYXYMgEagkFG6IIkyikClBQz0BnQRhM5gQ1iszmwmWiK3cznS6R8nmcIXCCCsmSs2B8WSWB0J-hs-hOIEKFyub3uqMwjwqLx5Hz5Xx+f00YmBoPBkOhPwofwA4kokSM+ejLFYrOknI47K4kpEMnZ0lYKYgrNEcVZAgtPMl8UTouzOdhLrBrrdeUYBXQAE6oADuyjYpGFnyhShhAAUxCIhGqUWjxhjMpkHP5MjZ0+4lo4MzmLQgaTZsM4UvrUv5tc4prl8hyzu7ud6Rb7ygHg6HuBHRVGRDC1CJeoNxsNk2NQGmMzNlniSY4AtEEsWM+5yzYV9rqfE7JlDq7mx6ve8Hp3sIGQ2H0H2jGKJAAREQDkFJ0aahA4o7uW2+PNWIkTrmmsJaRLi+rageARWoER5FCed78heV49uGbafII3y-P8UogmCEKvgqyqquOyIfqmlipNg6TWqkBz0ds5KgYEG7-tiSQ2Ac6ZWPBXKekhfqVFgUB3DItDIDImBobeGGilh4q4dKBFytGPxwgiMjvhqlFUrYc4Zi4+wrAezGUlmOI1tEK7RLsBrpM4dguo2bqIXJHZPMJmCiWA4lgJJ0k3khD4SoC+GykRkIkSI2kptOiBZJZvjEqa6ZpLsxYpDiGQ5qkdh2NSObOHxLYCe5yGefo3liRJUkycFClPi+8qxVOEwIOkSy4g5CQrCyBVFqB8Tfl4Bo2I4nXWSuJVuWefJCVeFAiXcDUCNhz5Ea1n4WI5G7OKyNauKk2RpP4xZHFEE2-iuNLRDYaS8S5x6tnNHkVIty1gKt2GhXhMqEfKsLwoiZHqnF7U7bqrg1lutoOekCzpHYxbEtEmw3Qa0TuPd40lf6tT4MgyBgJgEA0PNKEE0TJNk6MQJEHokA8CIj4DBIQICMoMZQgCYhAnwW26TtXHlgk8Q6t4Ga2cWV0xGaDkZodSTuHjVPE6T5NvQGas05rmD04zEDM6zPzxmoALKOIIgxgowIxaDk7bSktIkvlrL5Yc1nFvtLJOFmhzLAVRI2A2pxFPjNSE+rtMU55kAot5QKE5QYCoP6NTnEJWAoqgRBJ8gKdpxnPDxhzyiW2oj4SACCgxnwAIAEIiKCQI1woACagvxV+Ie6kcBrB-sqRmt7RKZLidYGhLnXuBmquR9TGvnnHEAJ1A+eF+nmcXtnpC5xvNxF+cPAUIIADq7Oc9zIi8-zEigtzXfta4s7ZKajnGTB0Tez4UQI+NrhPDbFsvPKOutl4VHjqQROydD5bwWjrUmIkkJ8HQFAc4RA0HnC9EfdgYBTC4BPoDMu19b58AkKXTmygn4YjrP4NG2osxi08CsMyiBnD4jLDmRwu47qARzJkUBi8Y5a2wFAkSB9U7wMpgvaOyDyqoPQZg9BOCt54IIUQshOEJC23buICgND2GARxONNwRx7AeDxN7WCNF8T0LSPlBGICnrh0QSIiqkDV7QPXrAqRGcEGyJpvI16mBFEYKwaojO6jCE8z5uQv498RzsAMQ7Ci3dXAizugWOi2IszSyGoHGIfgVypC3CHK0Qjo56yEuImBBc4H+JkWApB3kUFYOUdghp5xoltFEJIEhEgG5iAEGfKEFDBxUJEJXAE-1DEIHyjiBZtlEgHWyN-ApSMimHASFPew8RKngNjp4tekij4BOaavVpCj2kRK6T0gA0iIducYXwUEvlzQZwzRkiHGWXCuj4ZkETmVWcsvgsxI1sgsKY3tEqi1cBNewJIsbOTDjgCOFzqkXlqT4+pfjt6eXRcI4JPpQk3JUXc-BhCWZswGbE-mmhRxzPxGjeidYfD+3ht7bItIeEOUiAaZcj1UWXjcZileJzfFnIvAvTeUTyCwDUK8cqmi4naN0fouZRJEgxH3MZI4Oofyj2tHSLIXE0j7T8GaA5S8jl0Gxac6RnkZV3PlYq4KdL4kQgZckzVHtsAeDyorCa2ZViUmcBwkxpqLUWoyCrFxaLRUQLtV4iRkrHUVGdXi9grqlUhONjSq+coyHes1bETMSQjp4hpDqZGQ17obAyEsKYADqx2Gte4mpKa6myvxRUIgqBQhgH9F0hQ+BUCXKgDwAAYgIKQlc1UAj0WIFJOhyI6XSXdMs-swVORDvYTI3sswOHTBw+iWQvCOHbWK453iHWNM8v2wdw68WjvHSJHg6rJBqAED8Gdc7HyaqWA4ACCxvAsImussNNjaIIwKr+Aqlir1JrEV2nFPahKZtwfKhQA6h0KvwLgKd+AWCk3zT8WlN84kltSeu9qJ0oi7UiIZPMZozpDSmFEOIVlrR9U8II+NIrAk2tEfatN96M1ie6fK6JirCPEdI9S8jhaPXUdXWDNqaZFzlg8PLXYTCR5DVhcNFk2Nsh2jZAJwlVTkOidxVKzytACEjrHROng3qJDft-bOyugGFjYGxtaWyArsx1hhRkf14aOHWnuo5AqSHbUoYlXZ9NdBHORPOK+1zf750JNUyACcaTn6mlpPdM0FYHIrhrGxsNFanCwezL3AIc9LOJoS7Z9DF5qhgAANYbyEOgMAQZdD+hicQq+HrflULmV4XUpYJq2n3J1ew3tkhRCOOYnUKwjihybK4oTHasWobvb2ugXXevJ364N4bhC52CD6e8mMnyRljMoeXKZALZk0fBhiEkXCeGxbBeCq0MLbLlkctsRKdYsbxZE0dyTfpp1-BkKfeQJsHsermYScsK4OFZFntSKYob2GsjLHibUCNdh5g4XG4VVnDmw7XmIEmqBiZM7IP2moX0sUF1wKEJnmAWdgDZxzsAZHITsF5uwOuUooRzJ2oEJwnFCS7CW5y0C2YeEzFtGPLMVpTQw48cmxnzPWdgHZ6gTn5yiWtOoCNmM5xAywFF4p2EMhHyPiUNLgEsuvsafYZrhxgROrxBsjqNcRIoaBEci4OxKQDeduNwL035vLdNOt1AZQ6AIAQDuPbx3ou7sdCGc9n54zXv-MBeCOXB4aK2CNNaHhHg4ZrlsOPLGR7Fv6orPHw7ifBfC4t1zglbiRKZ+z7nh3qAnc8Eec8+MFA3nF++ZNt70zPtqcdrpfquJDiEitA5biLecxFLyVxeirIUW7YTft69RvvH8-72bkXVu5HeTHznsAeep+i45moKdMgxBVBlA3cPdrY+AZd7YN9CsMRrINxEhQM+M2Uw91cT1-UCpsQp5shEghUr9BMMUbNUMH9k9n9OswAesv8ndHwmBd5RRf9-9ADXd3dPdwDvdID8s11vtEBCxbE3BEhY1dhkU1xlg0ZWJTp+V9okge9xV78Tchcn9B8Ed-81BkdyEXczYLYrYbY7Y5cCc6R9hHISld8VhR5sx-UVgVgnJ9pL12QXgGB4Bxg3QCAWhyAhIGBmAwxmhuAyA5dAgogFsAgEZEhoZINLBiQYg4glwJoiRDQQ8SonCvChIqhah6hGhPDSg5dTQ0CsYnEqc-BCRiwLAwjYh4gWNoirR+NhV4jSg-Q5cHJaR-DlhIhrRbQQjMRJ5wjswphRoPB+UZoXoSUhIhRyofCsc0h1tJYYY8xixtQHA8p8dLCSQAg+iyoQkFpuwgphjfdPwUg6seF5trIzQMgFhpi3B-V7B5ieNTpljTwBiLwqofI-IAp6pNioDaMfsAgdUrQdw9kaR0gTjZjziaQFiriBNZpbiCVgwlpqovoXj2D1NPwOE6QWQgMlxOj6ED1QJijNh61OpsDiR9RrjBJOxaiQ46RDgrQJjbApihp4gdV9RxpfssgGQpD3pWsjADYncIAdCzQaIlxfwJoJpMY-jQJsDywMwVhbRsx1sWS79U1ktxM6AaD95JNNUKwHAKxbBnZDgcxWi9izj4MjgNTTRrDac2TDdEtb14c09X8oA2klFbks1KVgUjg5YnJlgUo4hQshp99cQL0iQmF+E20Wsb8CCksOsnVJNs0yB8MkJS0rQOi9xII8QsYf4sZ-NmRrpOEXAadcC6dhNzT2sukhJOQkIgRzhUBvJIA5l0wkh-VAJ6EFgdRqSw1khaRDQMwCopZPAZSLS5Twy+1cNn0j5MsRJS06xcRYDilxZylrEVhbF6slgvigzTSQy2s4d5STtsBMM1FsNBz8M5MSMuStjdIay0Yy1qdfBxplhZyNwYNAh0orocy3Q8yDtpC+yizpVIzpNKVZMiNDzqy8RgMDimNbBWza0w0kYogB4KxlccwAEezCy8UhI0tnM31vJfUDgyS8QtxbIzQ8Rqt2FIKItw0o9Oj4Lgz8C1ywyPzPIzs+sBsht8ARtMdsQeCiQAggN8RvYgswc8laIoJWEEL1z+ywA5kUg0ZTEAcAggdWjDgATfxfw8KVlL9nyzSE8ZCk85CU8h9IEec+dZCB9OcdCCxa8mikYzQlx8o1wHEtc4hqR6FPBFKhK+9iCFDrSgkbc05cAKDRLjzu5UCXAUp8cdR7zhTKQQ0HBGFT1bADgKkKLhFb9ezvIiCtKSDh99tR8s8P8fKALx56EJpaJ6FtdHA1weIZhmUsyKcFhnKNLH9tKhIzsfKqCGhMBN9XjOCSwwjvAG8cLOICoW9qJ7oDxt1TQEYdtVLVyGdarXLU9PJGA2AiAcq-L2p9xgNAISQIUZ4uL1dlgt0-B9otwWMeEaqRIUrDKdLfL2q-dMQ4g0ZWNEhzLAIHRrKiRbLxp9hDhoZlzcy1LiTlqMQLB+UaILF3YPAppiwZt9TjFtcDRIg+JajTiGjAjmj6ECj4g1ssxe5bBILnI8ggA */
   createMachine<MainMachineState>({
   context: {
     /**place cursor here and press CTRL+SHIFT+P and choose "XState: Open Visual Editor"*/
@@ -175,9 +175,9 @@ export const mainMachine =
       },
     },
     recommendationState: {
-      initial: 'noRecommendation',
+      initial: 'recommendationClosed',
       states: {
-        noRecommendation: {
+        recommendationClosed: {
           on: {
             EDIT_COMPSEARCH: {
               target: 'editingCompSearchState',
@@ -188,13 +188,106 @@ export const mainMachine =
           },
         },
         editingCompSearchState: {
-          initial: 'emptyCompSearch',
+          initial: 'initialCompSearch',
           states: {
-            emptyCompSearch: {},
+            initialCompSearch: {
+              on: {
+                RECOMMEND_ALPHABETICALLY: {
+                  cond: 'HAS_SELECTION',
+                  target: 'recommendingSelectionHighlightSearchText',
+                },
+                SHOW_COMPSEARCH_TIPS: {
+                  cond: 'HAS_NO_SELECTION',
+                  target: 'compSearchTipsNoSelection',
+                },
+              },
+            },
+            recommendingSelectionHighlightSearchText: {
+              on: {
+                SELECT_COMPSEARCH_RECOMM: {
+                  target: 'mainSelectionChanged',
+                },
+                SEARCH_IN_LAYERS: {
+                  target: 'layerSearchLoading',
+                },
+                SEARCH_IN_TEXTS: {
+                  target: 'textSearchLoading',
+                },
+                HOVER_COMP_BROWSE_RECOMMENDATION: {
+                  target: 'peekCompViewport',
+                },
+                KEYPRESS_COMP_BROWSE_RECOMMENDATION: {
+                  target: 'peekCompViewport',
+                },
+                EDIT_COMP_SEARCHTEXT: {
+                  target: 'initialCompSearch',
+                },
+              },
+            },
+            compSearchTipsNoSelection: {
+              on: {
+                SEARCH_IN_LAYERS: {
+                  target: 'layerSearchLoading',
+                },
+                SEARCH_IN_TEXTS: {
+                  target: 'textSearchLoading',
+                },
+                EDIT_COMP_SEARCHTEXT: {
+                  target: 'initialCompSearch',
+                },
+              },
+            },
+            mainSelectionChanged: {
+              type: 'final',
+            },
+            layerSearchLoading: {
+              on: {
+                FOUND_IN_LAYERS: {
+                  target: 'mainSelectionChanged',
+                },
+                LAYER_NOT_FOUND: {
+                  target: 'compSearchTipsLayersNotFound',
+                },
+              },
+            },
+            compSearchTipsLayersNotFound: {
+              on: {
+                EDIT_COMP_SEARCHTEXT: {
+                  target: 'initialCompSearch',
+                },
+              },
+            },
+            compSearchTipsTextNotFound: {
+              on: {
+                EDIT_COMP_SEARCHTEXT: {
+                  target: 'initialCompSearch',
+                },
+              },
+            },
+            textSearchLoading: {
+              on: {
+                TEXT_NOT_FOUND: {
+                  target: 'compSearchTipsTextNotFound',
+                },
+                FOUND_IN_TEXT: {
+                  target: 'mainSelectionChanged',
+                },
+              },
+            },
+            peekCompViewport: {
+              on: {
+                SELECT_COMPSEARCH_RECOMM: {
+                  target: 'mainSelectionChanged',
+                },
+                UNHOVER_COMP_BROWSE_RECOMMENDATION: {
+                  target: 'recommendingSelectionHighlightSearchText',
+                },
+              },
+            },
           },
           on: {
             FINISH_EDIT_COMPSEARCH: {
-              target: 'noRecommendation',
+              target: 'recommendationClosed',
             },
           },
         },
@@ -217,7 +310,7 @@ export const mainMachine =
             },
             recommendingMiddlePhrase: {
               on: {
-                HOVER_BROWSE_RECOMMENDATION: {
+                HOVER_BROWSE__RERECOMMENDATION: {
                   target: 'peekPhraseDefinition',
                 },
                 KEYPRESS_BROWSE_RECOMMENDATION: {
@@ -235,11 +328,13 @@ export const mainMachine =
                 },
               },
             },
-            fullPhrase: {},
+            fullPhrase: {
+              type: 'final',
+            },
           },
           on: {
             FINISH_EDIT_RENAMEREPLACE: {
-              target: 'noRecommendation',
+              target: 'recommendationClosed',
             },
           },
         },
