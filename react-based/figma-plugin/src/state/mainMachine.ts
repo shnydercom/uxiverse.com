a2af005b80254cbef8109183d30ee6313781fc63
shnyder <ajs.private@gmail.com>
2023-03-12 22:02:54 +0100

    streamlined host/plugin selection states

--- react-based/figma-plugin/src/state/mainMachine.ts
+++ react-based/figma-plugin/src/state/mainMachine.ts
@@ -93,7 +93,7 @@ export interface MainMachineState {
 }
 
 export const mainMachine =
-  /** @xstate-layout N4IgpgJg5mDOIC5QFsCGBLAdgOgC4Ht8AbXdABwGVdVcxsIwAzVAVxIBVCTyBiACQDyANQCiAJQD6AVQCSEkQBkRAWXkA5duIDaABgC6iUGXyx0pfJkMgAnogCMANgAsT7A-cBOAKwBmBzp8PFycAGhAAD0QfACY7bDsfAHYnDwcggA5op2jogF9csLQsPC5SSmpabFgyMABjdEZ0Ws5iMv5hcWk5RRV5AA0Zdl0DJBBjU3NLUdsERydE+M9ff0DgsMiERJ1XRK8daMCgnQ8fHy98wowcAAsTXAowIjrJqho6THwHp9rJ9op2CQUHoAYXYMgEagkwL4AEE1ABxETDKzjMzoCxWGYHOzpRYOdJOHxOOx2DzpdI+daIJwOBZOPY6Ow5HRbHx2c4FEBFG53L7PdGYV6VABOqAA7so2KQ+T8BTwgUpQRIAApiERCZGjVGTTGIHIJbA6I1EnzpRLRDypUIRezOHzYdIOHK+U4ePKc7nYW6we6PfkWIV0UUSqXoGW-BUiJVqER9Ib6FEmNEY0YbOb2x3O06BaJhLGOLyG42Es0Wq0XLlXL28v2ygMVOjIUPhgUUFgAIweuD+AMjoPBkOhcMRmqMSZ1qcQXiZDt20USRPmXkSBLzetNOjc7gJRJJZIpFc93t93xeDewTe4LYDHa78pBANV6tHY3HAqsG2n0VnXnni92K7Wli86bo4+KEsSpLkj4h5Vse16Cuel7SrWLy3mA3Z9gCMZxi+2rvpOCBfj+f4uABq42HqOTfmBO6QfuMEenBNanq257BhQWBQE8CE9oCD4DlCsIIkiCZam+KagBsNJ0gyTLRCyARMl4a4ILEyR4nRe7QbBxTwahbFvNgwpgGAABuqBELxgj-BIMgaOIML9hC-GKgCyhSAoYJ4RJUxSfYpybmyDjLDSTJMqpBwLtgsTOCklr4isuk8j6CGBsZpkWVZBkWHx9maGITlgi5WGAvZ8JKD5EwEf5sz7AsXhOj4Oghb4y4tZFOiOtgXiWtsTiMviXVOMl1apTliFGSZ5mWdZAi2QAIiIWFVcmfk2nVbJuCk6QJO4Kw6CplFqUSCzeAELVnM4+2jfprH1lNmWWZKV4TW2nYYTwUjKgtMKaBIHleTIWEDhQUgAEJAvGIxjtVknHQp6SbkaAQ0r+FpkVSalZLihy7Ka+KOnYo0mbU+DIMgYCYBANCGSKdTk5T1O0xYwJECYkA8CIC2DFCAjKMqQIwmI0KrROtWmri0RmnsDh2AE06NapAC0CSFhylzFKTjNUzTZ6PWTFO6yzmBsxzEBczzAJqmoMLKOIIjKgoTmiTDr5w+tGySzFMstfLZzsg4Ks4vaGuVlrDNG8z+uVJAaKYFAwLk5QYCoMKtTXIGPAAGL2cDfDyFbfMC0LIt8GLNXHcrZo9VjFoBNgS4tQpdiJGkiQk5HTN63TdBx6QCdJ8gKdpxn6VYGillDyP6fXDwarAvz9tqAtEgwgoyqwuDIhgsC68KAAmhX8MzCucTZmyvjEu4XhHRs0u343uzN4ybceB3TER4b3cm+l-dcdPB4o9M7ngnqQKeycgGz3lIIAA6sXQWIhhbQgkGCQWx9PZ6nSI-JuTpX7t1UjuB02Z2S7hvmHT02so49werHCA8dE6QNTrPdKVDu5cQQnwdAUBrhEG4dcX0wD2BgHCJhB8CDS4oIXkvDBupNipDcCSY4DgkjZDJHfewh1kZJF6k4JGsRX6d2-sbGOfd6ED0YcPKBY92Jd11hwiaXCeF8J4YI2ewjRH3mQQXeyEhnYH3EBQWRhF5Z7ENDojI+x5at0Ib1eISjUiqItNgoxOto692wP-QeTDgGsLsdTBx91MBON4fwtxGcPFiO8XZSEmg4xBLErDNaH5NFhICLsSJBiYnHWSLiOW8tEnJGSRQqsbCTEZKyZYmeNiDZpPoQnTh-CXECOsdcSp7RRCSEXgLCQ4MxACFgUCCQ0jlDL1+sVNQwTaqhO0R0vRUTX6qS2JufpyiknqNSdQ3+55JmAOYTM+mxiCkLMcUsspqz1kAGkRAHyfBQCgCDdn7MOSIY5UYl4iBXn9AcVyNo3PCXc-R0TEiqTdHEV5gy1EpM-jgMZ6TaFmIYX83JtigXzKgIs5x4L-lrJEd2bmvNtnKn4t4up0NEwexaXVNpET7ldJJcdEK34KUqKGR8mlGU2XfKMr8nJLDzyG2mWs8gsA1CfAml4suNS-EwgCWIBpbt8LwzTIdOIZI24slbmfSkirtiKIGaqqlIyv5zO1XQpleqAV0ENRCk1ZreKSJ8bU2M7AHUSuaSE112B3X+ESF63aPqZi3wcP6t5arqWa1pfkmhk1w0WOZfqoyMaeXsDjeaopltBX82FYmsVuKXXTmzWaXN+aEiqT2H0hJgbhkjQ1XSmtf9zEAMjSAoyRBUDWDAMKVZCh8CoHZTnAQUgV7Wv8YE3FMwPDHGIacWIwRpxuixq3QkhpkjeGyDibYH9K2atDaYzJS7slWJ5elddm7t08t3furiPAz2SDUAIAE2cj0r37fYZI9oWQpC8B+pG8xSXmmwBfO9LgH3uh-fOsNjL60rvSs2oRJqFAbq3aa-AuBs74BYNTTtAIhUirLn2xp7sM2gEvYyG9bJ35nHNIyOui4iO9PmBSXwiQFyfJ-v+3VwGWVNpXa2sgsBKlmvY5x7jArePdv49CQTjrfJSoOESbNb6uqOi6qaBVl7nBEZIVJ5cLd1PjIZQBiN2nG2VFoKInde6D1iokAhpDKGFpodmAuKWyl2RGmXCFDzmjHMUlvXYe9pJyPhyrVqzTgGpmrPShF8p1woMHuQ8e1evibPpvFni1LMV0sMiy8uVSAyFjEcK6R4rAX6W1uo8u0LUbsA1DAAAa2nkIdAYAxTGGFGItyEikFWpOcoZLz6S0JGSDh2IeGcuzHZB4CTJH6RjbndWqjwWaMzdXZUebS3k4rbWxt7sx7BCbMRXsg5Rz9uYvOTioTTrMEpZpPEVRZ3P34eOvJb8w2itunGwun5lWxBU1QJTfHZB121DAFnXOah86F15jbO2DsnYu2S9LfwMUUYklvm6IIQcq5qx6tj57kz8eYEJ2AYnpPyc-OHrgawwvRfi9QGTnjgJ2DC3YJvQqQIL16l-I3S051HSNTzW3LGJxvzZD0TSIIbdsMC4qwwuXROwAk8V5L2Z1COHUE28qa4opYBgGV8oGQC0FpKA1zCLX0O7PTB1+b-XE6Qq0lbjztMSR0dmiSFel0bdv2ld-V8+3FjHdi+dxLvJbKuLKHQBACATwfd+4D4DzoIOUUSDReDrFFztdqV1-FA3ifjcp-sBSM6lo9HYOcPXYNZW-0TLxwTp3Luyfl7mZX6vtewD19QP7ng0LYVqnhUi0HqKO+Q4hIdkf2ax-kkamow6qk2SuH1-3o3z67dz4dwvkvS+3eAtXwnKvGvOvX3bfAPReNQXOMQVQIPEPMPPgTXV2drSuNMS-fXcfW-KfTqVwPvBPV-Nud-ILIXL-BXZfc8T7Lff3BaJgMBOUcAyA6A4PUPR2eAiPRA8SSVGPNSFIbzGIZIFqYkPNLwdIOuAaVwZqfYPBIIeYYmDVZCdACg3-PuaXawF6MoEA-3WAHgReZUA+BBdgOMVBAQNFW2e2NURnYENgppDrL2ZcGKZ+Y4dkX8BcFwB-PYb8dnZJeYaIG6WQ0MBQv+ZQ1Q8gdQuAHgeyIEMQR8FgyPWzDgiWWw+cBkDwRw0iICKIW-HqI0JkMkLwnwn9OQ-w88RoIgJ4CAIIhQzQpaJQTQVeZ2WycPGIpAk+RAauBqU3LYbNfEVTZI2kbBQrUaAokI9KYo0o8okI2AKoLiYAhvHgKoneVFOoqIhA7vZqDohkFwU4c0bLIfBAQIBYLor1L1FcS0AYvwoYoo9AEoyAMYhvCY0wBOaY0AngGEEPFUaI5aZLPYtwM0Q41TY4jwB-BITDI0DYtPbY047gQooyEY64s424+gUUKAKALiBQ+eEQAQMQJaSQBoj4qPOIjaQIXEX8JIlI5wtI3Y0kdWEEwkMErLCE0gKEyoGEsouE0AiYiARE5EhOVEmQeEBDNUT4skHqaIEk2+VIh-EU3EFGUErYuk3wyE846Ey40Y1kjQi8VUgPF41eY9OgmQKA7mN45YvEkTY6V0OIOSTwrY9wU3dkIdJkEfekcfGQ-IjU4Y5U2EhU+EwYmYuY-6NQJ2KQf4WBIwnE5LZIwsMke0xHJ0h-R0osRkS07w9wek+QxUpk90lkz0tk9UrMnfXUqAw01gsM20yM6WaM8kOwB-E0TIhMnIq0hwFMxkugZkm47M70p4haQqeEQsxo9gk0jYcMu0ss07GM00i0Kk2sgkesxstM5sjM1stU9snfX01FIEcA0PCPJYos406wxAQc0sh0nDCs1wxwGs7IqcpMhsisD4BgeAUYbkJo2HZWLIFWM4XEPvY4M4RqBWK8n9AgVocgQMR8+zckxwhYRwPNTIFcOWY4Uaf87gcoIyBgZgKUFoBC4Cwic0AbOKLcNIZYAIKQ2dP80oQC88aoOoBoJoNCsoDC2qekDMJkBcMfT8s4bC+YPEbwPwAi4IW6Fif0SbWijaF81HZTRuY0fpXYSCRiH9O6fi9KD4BCQSjYdIG7U0ecA4I4K9Q4SKQkTcO5e5Pg6cXi8aIpVhcUIIxSvs3c2YHGRueSKS2KM0SKWIYE1GdzMsZwYyk8OSpCZsN6dCXAJSvUA0E4P4-8ZcEfAbN0b8K9AITwMCN0afMabyusSbYycUTiB48nCaIKzYXMBGMCRYVkFcXwEjLytKWxGabKIpXK1TQsbYMkFkRqEUwrYQhGA4Ik40V+FcA8DVWS1KvJKqiy-yj6QKqy5A+wK9d8+SCdVuf2TqJGeMokJVDGeYAggS8a51PUck58jo5-OKwrGIA4X8vPSjf9M6gUM2f3CAWq-KrEbgo49zTYxkDwdaxdELI1ICza2HHDQsEfIQkU86LYckj9eJUJBKIkFS6S06p7QvabT60BTASeIgBtDOXKkUlcQ0EUlqWWIIHDS7M+Hgy+MhVqN63HD66rVlf-DlUFLlVxCFPlXK3qDMHRTIXqPYYGp5BRFVd5CtGG8rD-V7BG3TN7fTVjSyqwiazYP1HED1Ykbww6accdGuBzZJR0XaEbMmnVSrVG97RsK4BCYEa4VABOSAXK0kWkRuDPK6MsmkAbY4XEBcTY5cL1ekLWuteGymtdZjCDYBBrLic2+kEtC3PG5uVTVi46K9G7fLGIEbe7LHR7AWwgnW2jA1PTRjH21jEzLjG676qVC2uka25qikO2yO8TGO0kBcPzRkd2qbIDYWyoejdxE1IzNjDjHO82qTeILIK9RIhcBzUld+CTIIecHPPmyhWGwWz2kDc8WrKLaDBOQOoQ+JbGlS-YEU+2w6Ye3qR0eYZIWul7aenTD7UyL7YeH7dbfATbJe4Onu+kMOqugbQ6s8wkVuIQy0ErCepOtKogkXRfMvBsJmxIG7OWA4K3eYZwbwJ+ktZ-EbI8p0XwA+3++XUvV3AIsgGXYvEgsAXKp0fYpkZa63SBo6LEXvePLqAfZPJB+fP+7-AB93dhBZL3XABQ829pGKJIPwMkSMgIO62PPXS0XApPfAxO2fZOz-Wh7BlfD3AA9fR4-3QOmuPRNIHoxwbYCkSKMhwRihvAk6r+sRn+mhlBn-dKcgkIqgxoJGjrGHezeWVwIQuqi0QISuwtba1S29Pg5wOaoQ6hiR4x+hpktgIgVhvOwiEUx+EkbGrIbYKJTRhYfLaWMfNGV6+Uhk2c9Gnat++MuK4kLqY4T+qsJcxQzJQI0MMgcY26uJ78KJrx789SmchvN0q4zMtJ243KlSyKE4exo0YBxqpGXYJKoppplU3MuASYrKkJyWra2YZejyrYrI0qh-RSGsmU7wuUl00Z4Zj01p7MsAZQ3B3aIjLopGNIThgIJZnIeM9qBB5I50vPIZi45phcsZjk1AJElEkI82pIKsy5lGBSHNU0RB1J1Mxpx5kZnZxcjUg58+Y5lqUK5qVx3Y5xs8xMnw-IIAA */
+  /** @xstate-layout N4IgpgJg5mDOIC5QFsCGBLAdgOgC4Ht8AbXdABwGVdVcxsIwAzVAVxIBVCTyBiACQDyANQCiAJQD6AVQCSEkQBkRAWXkA5duIDaABgC6iUGXyx0pfJkMgAHogBMAZgCM2BwE4PbhwDYn7n3YANCAAnohOAKwAHHbYEU7eAOyRkQ6JngC+GcFoWHhcpJTUtNiwZGAAxuiM6BWcxIX8wuLScooq8gAaMuy6BkggxqbmlgO2CHYALN7Ybk5TiQ6TOk5uUZOJ3sFhCE6TUTMJ3t4xOok6k27HWTkYOAAWJrgUYESVI1Q0dABOqADuyjYpBebwqIx4FHaAGF2BIAApiERCPpWIZmdAWKzjJxRXGuNJpCKTZbzRLbexnCLYZLHaKJCI+NwXG4gXIPJ4g94YzCfEq-AFA9CcsHciHQ2FqESdXr6VEmdGYsbhXFRfGLenElZ2MmhexORKxHTHJJOBKXbwstnYR6wZ6vLkWXl0TD4YXgwQUWEyDTiACCMJkAjUEkhShhEmUUgU7BkKIGaJGWMQDnW2ASU28U3iaTW5ImdjsMy8DhLzkmESuZ0td2tHPtIsdxWdrvr7oEnok3s0Yn9MaDIfFIe9AHElHGjPLE0qECnJmnM9Ms350lE8zidKrLkdJnYmW47Do3NW8t9KvhkMgwJgIDRuU7sKeKufL9fbxYoUQTJAeCIACI9CQoQEZQ4UhX0xChPhx0GSduSTGcLlmBd0h3AsUycPNEiw1wdAiOlpicQ8ogiY8cEfZ8rxvD4mwfM8L0ot9MA-L8IB-f9YURNRfWUcQRDhBR-REaCEzg6cHCmJCFjcVDHCiDDdQmbw3GwSYjjWBwIm8HQS3pUjaKfejX2or5sEgdFMCgKFz0oMBUG+Cp7idHgADFvRkCg+HkdjAOA0CRHAyDhNgxVQHGMtJI2aSC1k+SdgNGYolwiJkmSiIC3WPTyMMqi7xoszSAsqzkBsuyHPvLB0VQIgipK+z7h4REgOUHi1F-CRfQUOE+F9AAhEQYyhDqFAATSC4ZRNC+xxMSOICTcPDNL2VcFPmK552OYjxLktYnEyuiXxyxsTPyrBLOsl5SscmiKtIKqaouuqIUEAB1HyQLAiDPJjUCxoVUZJomNIZoCEsN13ZK8wiDc4h0WHdx3QjokmPaDIOxj7xOwrztsur7yyg7Trdbk+HQKB7iIUn7jtS72DAaxcDFMNYSavyAs8xrgOUX6pwBwjiNcS4YhxaZVOiNd9SpVSNsWKGjWklGKKM3LjogcyzuKh6ypo-HKMJ1ticpimyepurafpsU2c7YMBOG8QKG5iabHCDcqSWNY7GF7xReWnZiJ0dbzmihkNyPbJWRrHWlaOkpMfV2qtZMyPVYsomLBJsmjapzX7jNhmPsgq2JE0aV7dleNgv+p3dhdgX3c97283mlwjnOM45J0bVQ9uE99oY4yY9Vgq4+zvHe+vPXQRGdPycpk2HNzppREkFmJB6sQBGeyEJA55qRFa30+zUB2QqrvnXcFj31i9yIffCQjYmNE4GTsba5IV7L0bywfTvunGE75MeycoCp0wNPTOc8c50wZgAaREMNBEIgKAUDenCVe69N4iG3iIJqLVfwH0DEfMuE5xon2xDXN2Qsr4NxWrhWIRxjjSR3Ftd+aN+50Fjr-S6o9Ua6xTvrNOhtZ7ZwXn+ACK9858GLjKfoxC-rwTPrXShIsb55iSMpekcNPAHH2BaMOVok6fxVmrThuMaIGXjjncgsA1AtknqKCRhcbZ22PpXMh-MKGX2UWLBS6RlLGkDtJRIxIvAsL7srAexjsZcLMVE02VibEgItp9QuUjS4yJgiQ1xzt3EX3riohSTJ-aPx2nJTS3gHChKjjyL+kSNZ-yuiZcxwj4m2IdJgNiYjfIDjZlIlx8jyG5KofknYmZlJ2FltqdwcwoiJEqYdapRih4mP-nQIgqAQhgG+NnBQ+BUBAJcgIKQrVHG+ltmINJcpMnyOSC4dYHgPYpiSMRWKiB5qGlhhcSk9JvAMjmYYiJSzYkrOwGsjZWz6k7L2adHgTjJBqAELCZyhzWp9OnPqU02A7n7j8AcRIzy8yJX9rLDu6RNhYUWH8thplv5YzqdExpQLLFkFgAodZmzrH4FwM5fALBrwdOZl0iRvSiEZLkdORw2pZqLHmnhXw+wCWwxhnDUlSQsIVL0RHQB-z2E0uHvU+8TT6nsCsbnGxXKeV8tEQKkC3TPrCvSSJUhU1JUMmlQtOVt8EBMhcMSkWmZCJzEpeEnVtSLH3loPTbZuz9lSIkPCxFyLfyot5jczFlxsWPLxfERuKwlUd32DSeayMNU9x4VUjGurlkNJKBGiBkL9lIqOW1b0RcpTSMuWKlNGKsUPNxfimhHc81+EXFpKIQbo4hsBXS0xJlyhgAANY1SEOgMAfxjDfDzoOFmDid5cxFY6rJuxU09pxU87NNCiTYEfkpYkEqx0lrIlqqlHDGX3jnYu6yy7V3roZkcwQS8UFoI3lvXde88GH2TafEOaZCRA32M-IIA6XDFKSGkF+vyH36UVvMitasxBXlQJefDZA1kVDAE5Vyah3KeStVgriPFET8UEpBsKEkrhSRkuhPMl9ZhHBiBsFY5SvbjoWQC06+HMCEbAMR0j5G8rFVwCECTUmZOoDI-ykM7BwLsC6j2SELHEAHFiGsRYFxoinA7txhw-t5hQwSIefYql1Xd0fWWnDNSh7KaI2AEjam5OJ0AYTagG64T3F+LAMAGnlAyF-L+JQunfT6f3RXeR4k5xeHih4GZ5TphWZs+MwTDn1h+BE7hzzBHvO+bI9w7Dp1lDoAgBAN4oXwuRf-S0NewHMFYNA-vCDyWrlorkqqfNQt0gFiJDqHY2oXAfJWIlYkkxCSlY8+Jir0mfOyZq9lOrDWmtgBa6gCLPBYHwMREgoDGCsE4LA-goMBndjDewKNj243xkbG4wePNBYNzDa1CtxZa3JOVa29rQLFl6uNea2Fo7kWgJqFcmIVQ0XYvxb4HpoSA3O2nyey9-U+53tTeTPsPN4knmbBWMWlzWGP7Pt1V5jbVX-MlHfYdiLv4mA3VFPDxHyOYtxb4ujxLmOHUpfFf4akBXojaRLF4CIn2ZoaMp1hQiOJ73U+QIKNnzP2EKZCICbgZAYcRdgDwICcJhooPYNKIuAg6PcV4kxqEIuO08yrjNq96biK+BxO4RD03UrYELEpBbmZtS4j0pr7g2uMZ64N4UY3cAeDekhGIWECWkui8GwDF+yGveLV9-ubjaU5ybCSPEDwKYKWYaj6QGPNEahEDeBAeP6Btem9-O0TQbUBIdgzy78u2f3dyU91lgvUQ-fcY2HOYPixfAblhs58OeRa9t8T-eRvzfW-t9KKdaHrWeCd6UJoCQvf09C8z67x2YUMWLn2F4VYWkGTcf3MpFY5wNjxDxUEyPWv18N-QCb0gG30T1gF3wsn31hx4F9Fi3hAv0QQe2cBcDvyiAf0rGfwUms2s2ewSA8AXHml8F-2j3-xMk32AL-1azAIgF+CgCgFOm1wahEAEDEE70kH7wuUH2xxv2QIXFQOcHQIcDzGs0SjiASFxArD2DwiNCILrxIJKDIJbwoNhyoJoLoIsgYJkGHHhUREQNv14LQKf0EMwL8Fm1wOCTkimAnxkLX1aw30AK3yUJN2wFXwYJgLaiOR5xkCRz-DgIxwe0DjTCmHuSuB8F8CENNBmjwkLGmC0h2h8GsPr1IPsPIOIMoOcMcMiyP36kwTUH4ikE9Gejt3738MHXKXmFwnmCSHGSEKNBmloW+UYRiHiJrwyLsKAMUNSOUPSM6OO08KR18OF38NUkxWcHGVWESiLX92TEEziDSiUm-wsN0Q11aIAPaJALSJcMT0Px7GHAGMv04Ld3GChjnCUkr3LDGNUjcHCJHy-1hmWEimmASLkLoAUPWK6M2IPyyJP0hHhzi0S3Pz8Kx0OOTBH2WFhn1ANCuHKSMOmzcHSFEOSCNCRmSgSD0htDtDsQnQfH+AoD33I34XaQ9C9B9B7ADH7FDGwVhEjGjFjCBOv2dihiDyCQ8A7jwh3A+wUgPAnyD2iBLELAJCWDRLrExNEzoFXxAQoBYAACMXgGYiSrZuxewCEBwmYhw1BRwB9ZFgSJhywZgq8dw8U-AjRqiVodoVJA4CQP9iIqdl92RbQJSaJxSCTJSZSwA5T2xiTFSyTgwKTwxqSYwHsPYvtfBHB4hJkZl5dOTCxNx6Elgmj4ghT7TnTtYcS8TEl5Suw-RvSVTKS1SNSHtEZ-ZtR01WSiQFg1wvBDQpYtJHArhVJZlMN0SHSTInSRSXTZSmgOxMzSTD4czwwKARwxwnAs8uDlQ8QdJCRNRSQ1w8VVRQjjhlgJjpgSIWQXQGB4ABg2Qr8nUEAABaKYPMXc4YzwE808jwPSAgBocgJ0bcw9A8ladwVUR+Cw9YcsPCC8goa8miBgZgIEeoQ3W8+CA0NceIKkCWC4QiJbQldXW0-IK8ooEyMoSoaoWof8woQC6cDYWIQsKXF2KEvCEC-UK9DRVSC4FMWGGCq0Js5Mr4DCnPSYGcxVA4Y0HEDUfYOwRMjEtpe8F0EBOiquYClaGIB+Y0OkR-RYJfKi4U7ilMgUbgPig4+k3YRaNMZk-cc4ZIdSbjIiIPR+bSDwHcTi5skoVs7i6U2U-i8YcZNKTFGZfYI0MEtYLYTksGTFA8RwcpYiCfIkIymivkVMiA-EkUyyxAA0BwNMeYafH5YrF5R7aGas2VUlZo6nAxNhEKnUvMcsPNOGEPIM4iAHABNzRiZiCLCAdKwS6bbAmIk4MKisDMBslKp9YNalUNEeJsdKzScKuYDyxwEWSzBSBGZ7D5F+cpOzTYAqydH+V9a6TASqaqRldK8PGaCw6YazdML2KYhAE4OcM4SQmICw2cCalqqdMNMHNzCeNpMBIRI1KBdKn3XjHqpYL2fqkZNaUsHwCWaSVII6l9adYFQ1GmFpBSrUpS+KZ7GZazY0RwaaSGVYZ7CZGq1-DYH6ytaalsu4EBKEe4VACySARa7SQ0LLANNKK4T1QiI0VwYlNKVaVAyizVIqunVq-VGiUFTZKNKFCydKyIbSVSy4clcpDuHEBVGaKWG5ba3KlGpm+lEoAGuJZlVlMFDlc1XlMqxSncg8cSZ7Imw8Emk4BVWbYalVclSS+m7DbVY6qav66tOgWW+eE1KBM1blFW-GlMFSI0MGDSCVPYHNd5OGAtXAnyzDVK5q3606kyWtdmoBLm44ymsPK4aVV-HNGMhhVSA4WGBq2C4OrE0Otq2dMABdJdFdNdfADdaOnm-UPmrCAWy+NcJKdaFOphF+SW8rYHRnUHWitWw9CsVUF+Wq8ScSA8ZynYPwcKubB5QWv3JYzOpq7O+ndbVTareTMgRTBnBesAO66YIPGZUM-un7IeqaIpArezaSYrE20tM2xmlulTTbPzbbAmFOYLXAbXLmykcGsKotZIdYGEg+q9I+2IxzErIOme0Ui2iyVem+xegLc6iHPbSAiLaOrK4srCU4FMXcb+wGQ+uzf+0+5uoHa+pnN9fO+dbXDnGoWat3A9eCS+cKqZOYWcRKdYSM6bF+Hk8EjYREtXXBsB+eiBnXbARgNgIgZ+zuqhn5H1DRZ+fUahqzE440bejSaEksJ42w9qkR8VBizkx5akWkGZOSHSVElonovhsAOPQUI3Sg8qzayw8KvYKR-cdYaI5R2HNohwoxjckGnc1A7jdYf2MlLkrwI0L+pxiLFxlI2QtI0wQK4Rjxw9G+K9ObH5H5b1LCF-EQ9-QW3ENICWYJvh14jIsAkx5enYGJoCpSc0uzcSCvaVF-Fhwsc4MPDcYdKeq0D45x1Y1x8Jro6g1AWg+gxPLmtIcIpYHAm9QWSKqwwxzpkJ9psJmw94jI8qspz-FYSp1Yap4wxYeG7UXCJIMR3aLIDIIAA */
   createMachine<MainMachineState>({
     context: {
       /**place cursor here and press CTRL+SHIFT+P and choose "XState: Open Visual Editor"*/
@@ -153,73 +153,74 @@ export const mainMachine =
               src: 'checkForFigmaDocMessages',
             },
             on: {
-              HOST_SELECTION_CHANGE: {
-                actions: "assignHostUserSelection",
-                target: 'reevalSelection',
+              HOST_INTERACTION_SELECT_MULTI: {
+                target: "rawMultiSelection",
+                actions: "assignHostUserSelection"
               },
+
+              HOST_INTERACTION_SELECT_SINGLE: {
+                target: "rawSingleSelection",
+                actions: "assignHostUserSelection"
+              }
             },
           },
+
           rawMultiSelection: {
             invoke: {
               src: 'checkForFigmaDocMessages',
             },
             on: {
               SELECT_PREV: {
-                target: 'reevalMultiSelectionSubSet',
+                target: "multiSelectionSubSet",
+                actions: "assignPluginSingleSelect"
               },
               SELECT_NEXT: {
-                target: 'reevalMultiSelectionSubSet',
-              },
+                target: "multiSelectionSubSet",
+                actions: "assignPluginSingleSelect"
+              }
             },
           },
+
           multiSelectionSubSet: {
             on: {
-              HOST_SELECTION_CHANGE: {
-                actions: "assignHostUserSelection",
-                target: 'reevalSelection',
-              },
-              SELECT_PREV: {
-                target: 'reevalMultiSelectionSubSet',
-              },
-              SELECT_NEXT: {
-                target: 'reevalMultiSelectionSubSet',
-              },
+              HOST_INTERACTION_SELECT_SINGLE: [{
+                target: "multiSelectionSubSet",
+                cond: "isSelectionInSubSet",
+                internal: true,
+                actions: "updateSelectionSubSet"
+              }, {
+                cond: "isNotSelectionInSubSet",
+                target: "rawSingleSelection"
+              }],
+
+              HOST_INTERACTION_SELECT_MULTI: {
+                target: "rawMultiSelection",
+                actions: "assignHostUserSelection"
+              }
             },
+
+            invoke: {
+              src: "checkForFigmaDocMessages"
+            }
           },
+
           rawSingleSelection: {
             invoke: {
-              src: 'checkForFigmaDocMessages',
+              src: 'checkForFigmaDocMessages'
             },
-            on: {
-              HOST_SELECTION_CHANGE: {
-                actions: "assignHostUserSelection",
-                target: 'reevalSelection',
-              },
-            },
-          },
-          reevalSelection: {
+
             on: {
               HOST_INTERACTION_SELECT_MULTI: {
-                actions: 'assignHostUserSelection',
-                target: 'rawMultiSelection',
+                target: "rawMultiSelection",
+                actions: "assignHostUserSelection"
               },
+
               HOST_INTERACTION_SELECT_SINGLE: {
-                actions: 'assignHostUserSelection',
-                target: 'rawSingleSelection',
-              },
-              HOST_DESELECT: {
-                actions: 'assignHostUserSelection',
-                target: 'noSelection',
-              },
-            },
-          },
-          reevalMultiSelectionSubSet: {
-            on: {
-              UPDATE_MULTISELECTIONSUBSET: {
-                target: 'multiSelectionSubSet',
-              },
-            },
-          },
+                cond: "isNotSelectionInSubSet",
+                target: "multiSelectionSubSet"
+              }
+            }
+          }
         },
       },
       recommendationState: {
