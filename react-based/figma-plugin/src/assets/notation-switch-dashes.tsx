c6e3dc12dc7daf84d5ca7a345be760ef68a664ca
shnyder <ajs.private@gmail.com>
2023-04-01 19:08:53 +0200

    added new icons for notation-switchers

---
+++ react-based/figma-plugin/src/assets/notation-switch-dashes.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const NotationSwitchDashesIcon = () => {
+  return (
+    <svg
+      width="24"
+      height="28"
+      viewBox="0 0 24 28"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M6 14L10 14" stroke="currentColor" />
+      <path d="M14 14H18" stroke="currentColor" />
+    </svg>
+  )
+}
---
+++ react-based/figma-plugin/src/assets/notation-switch-slashes.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const NotationSwitchSlashesIcon = () => {
+  return (
+    <svg
+      width="24"
+      height="28"
+      viewBox="0 0 24 28"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M15 20L18 8" stroke="currentColor" />
+      <path d="M8 20L11 8" stroke="currentColor" />
+    </svg>
+  )
+}
