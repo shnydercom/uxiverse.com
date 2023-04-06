badea824239536e6490c36fa4077e697b4149f9c
shnyder <ajs.private@gmail.com>
2023-04-06 17:56:21 +0200

    useful changes

--- transformations/jsonld-tools/src/createGraph.test.ts
+++ transformations/jsonld-tools/src/createGraph.test.ts
@@ -1,7 +1,21 @@
 import { describe, expect, test } from '@jest/globals';
 import { createGraph } from './createGraph';
+import * as  uxiverseOntologyJSONLDfile from "@uxiverse.com/ontology/ontology/uxiverse.com.json";
+import JsonLdProcessor from 'jsonld';
+import jsonLdLint from "jsonld-lint"
 
-describe('creates a basic graph', () => {
+describe('given empty input object', () => {
+    test('then should return empty output object', async () => {
+        expect(createGraph({})).toStrictEqual({});
+    });
+});
+describe('given flattened json-ld without blank nodes and no language specifics', () => {
+    test('takes an empty POJO and returns an empty POJO', async () => {
+        expect(createGraph({})).toStrictEqual({});
+    });
+});
+
+describe('given flattened json-ld including blank nodes and no language specifics', () => {
     test('takes an empty POJO and returns an empty POJO', () => {
         expect(createGraph({})).toStrictEqual({});
     });
@@ -20,6 +34,18 @@ describe('creates a basic graph', () => {
 // objects should have arbitrary keys that resolve to arbitrary graph nodes
 
 // context should be handled
-// local "orphans" like "@id": "_:b0" should have a place
+// blank node identifiers like "@id": "_:b0" should have a place
 // nodes with "http://..." should have a place
 // nodes with a namespace like "uxi:..." should have a place
+
+describe('given flattened uxiverse json-ld without blank nodes and no language specifics', () => {
+    test('takes an empty POJO and returns an empty POJO', async () => {
+
+        const getProcessedUxiverse = await jsonLdLint.process(JSON.stringify(uxiverseOntologyJSONLDfile))
+        const getUxiverseFlattened = () => JsonLdProcessor.flatten(uxiverseOntologyJSONLDfile as any)
+        const flattenedLD = await getUxiverseFlattened()
+        expect(createGraph({})).toStrictEqual({});
+    });
+});
+
+export default {}
