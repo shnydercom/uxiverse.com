ca586648ff519a6e4ca7a1f544db83c2d7f9e8a7
shnyder <ajs.private@gmail.com>
2023-08-31 17:52:08 +0200

    nodes are found in ontology

--- react-based/ontology-documentation-in-browser/ontology.config.js
+++ react-based/ontology-documentation-in-browser/ontology.config.js
@@ -1,6 +1,16 @@
 const ontologyConfig = {
+    /**
+     * the IRI that serves as the base for the json-ld schema
+     */
     baseIRI: 'https://uxiverse.com/ontology/',
-    baseNextJsPath: '/ontology'
+    /**
+     * wich path nextjs should use as base path (entire /src behind this path)
+     */
+    baseNextJsPath: '/ontology',
+    /**
+     * the location where the schema defined as json-ld can be fetched from
+     */
+    jsonldIRI: 'https://uxiverse.com/api/masterdata/uxiverse-flattened'
 }
 
 module.exports = ontologyConfig
--- react-based/ontology-documentation-in-browser/src/app/[definedTerm]/page.tsx
+++ react-based/ontology-documentation-in-browser/src/app/[definedTerm]/page.tsx
@@ -1,10 +1,46 @@
 import { i18nEN } from "@/i18n";
 import { Box, Typography } from "@mui/material";
+import { createGraph, findIdentifiableNode } from "@uxiverse.com/jsonld-tools";
+import * as ontologyConfig from "../../../ontology.config.js";
+import { notFound } from "next/navigation.js";
 
-export default function Page({ params }: { params: { definedTerm: string } }) {
+export const dynamicParams = false;
+
+async function getOntologyGraph() {
+    const fullOntology = await fetch(ontologyConfig.jsonldIRI).then((res) => res.json());
+    return createGraph(fullOntology);
+}
+
+/**this reroutes to not-found page for terms which are not matched statically */
+export async function generateStaticParams() {
+    const graph = await getOntologyGraph();
+    /**
+     * only terms that start with the baseIRI will be documented
+     */
+    const baseIriLength = ontologyConfig.baseIRI.length;
+    return graph.identifiableNodes
+        .filter((val) => val["@id"].startsWith(ontologyConfig.baseIRI))
+        .map((identifiableNode) => ({
+            definedTerm: identifiableNode["@id"].slice(baseIriLength)
+        }))
+}
+
+const getNodeForTerm = async (term: string) => {
+    const graph = await getOntologyGraph();
+    const searchIri = `${ontologyConfig.baseIRI}${term}`
+    const nodeForTerm = findIdentifiableNode(graph, searchIri);
+    if (!nodeForTerm) {
+        notFound()
+    }
+    return nodeForTerm
+}
+
+export default async function Page({ params }: { params: { definedTerm: string } }) {
+    const nodeForTerm = await getNodeForTerm(params.definedTerm);
     return <Box>
         <Typography variant="h4" >
             {params.definedTerm}
+            {(nodeForTerm?.["@t"]?.iri) ?? "" + "unknown"}
         </Typography>
         <Typography variant="caption" fontStyle={"italic"} >
             {i18nEN.ONTOLOGY_TYPE_SUBTITLE}
