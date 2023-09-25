b086e64d4b96e8373d09f09648d6af8c33eae341
shnyder <ajs.private@gmail.com>
2023-09-25 21:49:04 +0200

    experiment to load schema.org worked

--- react-based/ontology-documentation-in-browser/ontology.config.js
+++ react-based/ontology-documentation-in-browser/ontology.config.js
@@ -3,6 +3,7 @@ const ontologyConfig = {
      * the IRI that serves as the base for the json-ld schema
      */
     baseIRI: 'https://uxiverse.com/ontology/',
+    //baseIRI: 'https://schema.org/',
     /**
      * wich path nextjs should use as base path (entire /src behind this path)
      */
@@ -11,6 +12,7 @@ const ontologyConfig = {
      * the location where the schema defined as json-ld can be fetched from
      */
     jsonldIRI: 'https://uxiverse.com/api/masterdata/uxiverse-flattened'
+    //jsonldIRI: 'https://schema.org/version/latest/schemaorg-current-https.jsonld',
 }
 
 module.exports = ontologyConfig
--- react-based/ontology-documentation-in-browser/src/graph-logic/getGraph.ts
+++ react-based/ontology-documentation-in-browser/src/graph-logic/getGraph.ts
@@ -1,9 +1,15 @@
 import { RtLdGraph, createGraph } from "@uxiverse.com/jsonld-tools";
 import * as ontologyConfig from "../../ontology.config.js";
+import * as JSONLD from "jsonld";
 
 export async function getOntologyGraph() {
     const fullOntology = await fetch(ontologyConfig.jsonldIRI).then((res) => res.json());
-    return createGraph(fullOntology);
+    if (ontologyConfig.jsonldIRI.includes("flattened")) {
+        //early return when resource is explicitly pre-formatted
+        return createGraph(fullOntology);
+    }
+    const flattenedGraph = await JSONLD.flatten(fullOntology);
+    return createGraph(flattenedGraph);
 }
 
 export const isRtLdGraph = (input: unknown): input is RtLdGraph => {
